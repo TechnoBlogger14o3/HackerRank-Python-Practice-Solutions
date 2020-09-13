755eb1e04d5c8adc66e59d3f39412dd32b1603c7
Aman Shekhar <Aman Shekhar>
2020-09-13 08:36:25 +0530

    'capitalize-python-practice-solutions-hacker-rank'

--- hacker_rank_solutions/23. Capitalize.py
+++ hacker_rank_solutions/23. Capitalize.py
@@ -7,14 +7,20 @@ import re
 import sys
 
 
+# Complete the solve function below.
 def solve(s):
-    a = s.title()
-    return a
+    for i in s.split():
+        s = s.replace(i, i.capitalize())
+    return s
 
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
     s = input()
+
     result = solve(s)
+
     fptr.write(result + '\n')
+
     fptr.close()
