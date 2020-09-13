b1b2cf6f717a7f198360dbca0d04356bdb271053
Aman Shekhar <Aman Shekhar>
2020-09-13 10:43:45 +0530

    swap-case-python-practice-solutions-hacker-rank'

---
+++ hacker_rank_solutions/92. sWAP cASE.py
@@ -0,0 +1,14 @@
+def swap_case(s):
+    x = ""
+    for let in s:
+        if let.isupper() == True:
+            x += (let.lower())
+        else:
+            x += (let.upper())
+    return x
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
