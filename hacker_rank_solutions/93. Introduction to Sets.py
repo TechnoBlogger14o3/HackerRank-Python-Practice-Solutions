bd1b31ffd9dc3920cee89cb2c5358ccf81591e3f
Aman Shekhar <Aman Shekhar>
2020-09-13 10:44:54 +0530

    introduction-to-sets-python-practice-solutions-hacker-rank'

---
+++ hacker_rank_solutions/93. Introduction to Sets.py
@@ -0,0 +1,9 @@
+def average(heights):
+    heights = set(heights)
+    return sum(heights) / len(heights)
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
