52986c6552e3e72536bb8e077bd745117ffb1ea2
Aman Shekhar <Aman Shekhar>
2020-09-13 10:46:06 +0530

    re-splits-python-practice-solutions-hacker-rank'

--- hacker_rank_solutions/93. Introduction to Sets.py
+++ hacker_rank_solutions/93. Introduction to Sets.py
@@ -2,6 +2,7 @@ def average(heights):
     heights = set(heights)
     return sum(heights) / len(heights)
 
+
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
