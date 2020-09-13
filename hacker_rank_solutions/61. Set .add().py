19c8f10f94cc7436f5fb75bf48b5b129024e9064
Aman Shekhar <Aman Shekhar>
2020-09-13 09:58:43 +0530

    set-add-python-practice-solutions-hacker-rank'

---
+++ hacker_rank_solutions/61. Set .add().py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    N = int(input().strip())
+    stamps = set()
+
+    for _ in range(N):
+        stamp = input().strip()
+        stamps.add(stamp)
+
+    print(len(stamps))
