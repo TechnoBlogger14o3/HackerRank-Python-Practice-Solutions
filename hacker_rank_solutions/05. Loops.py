d2c3d91afeb45ee30cafcb91aea1ec909d56da97
Aman Shekhar <Aman Shekhar>
2020-09-13 07:47:02 +0530

    'hacker-rank-python-practice-solutions'

---
+++ hacker_rank_solutions/05. Loops.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    i = 0
+
+    while i is not n:
+        print(i*i)
+        i = i + 1
---
+++ hacker_rank_solutions/09. Find the Runner-Up Score!.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+    x = set(arr)
+    x = list(x)
+    x.sort(reverse=True)
+    print(x[1])
